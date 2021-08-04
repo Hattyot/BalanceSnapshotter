50d76dcfff0af47048f5f98db0179b08df8745ee
hattyot <hattyot@gmail.com>
2021-08-05 00:18:48 +0300

    Upgrade to version 0.2 * Updated typing and documentation * Added MIT license

--- src/balance_snapshotter/__init__.py
+++ src/balance_snapshotter/__init__.py
@@ -1,19 +1,43 @@
+"""BalanceSnappshotter - module for eth-brownie used to take snapshots of token balances of accounts."""
 import asyncio
 from tabulate import tabulate
 from typing import Union
 from rich.console import Console
-from brownie import *
+from brownie import (
+    interface,
+    accounts as b_accounts
+)
 
 console = Console()
 
 
-def val(amount=0, decimals=18):
+def val(amount: int = 0, decimals: int = 18) -> str:
+    """
+    Convert token amount to appropriate string value.
+
+    Parameters
+    ----------
+    amount: :class:`int`
+        The amount of tokens.
+    decimals: :class:`int`
+        The decimals of the token.
+
+    Returns
+    -------
+    :class:`str`
+        amount of tokens divided by 10 to the power of `decimals`
+    """
     return "{:,.18f}".format(amount / 10 ** decimals)
 
 
 class Token:
     """
     Class used for making certain values of a token more easily accessible.
+
+    Parameters
+    ----------
+    token: :class:`brownie.network.contract.Contract`
+        Token in brownie form.
 
     Attributes
     -----------
@@ -28,8 +52,8 @@ class Token:
     decimals: :class:`int`
         Decimals of the token.
     """
+
     def __init__(self, token):
-        """Constructor for the Token class."""
         self._token = token
 
         self.address = self._token.address
@@ -37,16 +61,22 @@ class Token:
         self.symbol = self._token.symbol()
         self.decimals = self._token.decimals()
 
-    def balanceOf(self, *args, **kwargs):
-        """Redirects the balanceOf function call to the actual token."""
+    def balanceOf(self, *args, **kwargs) -> float:
+        """Redirect the balanceOf function call to the actual token."""
         return self._token.balanceOf(*args, **kwargs)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         """Magic method for hashing the token class."""
         return hash(self.address)
 
-    def __eq__(self, other):
-        """Checks if token is equal to other value."""
+    def __eq__(self, other) -> bool:
+        """
+        Check if token is equal to other token.
+
+        In the case of a string it checks against token address.
+        In the case of a Token type, it checks against self.
+        Otherwise it checks against _token.
+        """
         if type(other) == str:
             return self.address == str
         elif type(other) == Token:
@@ -57,7 +87,16 @@ class Token:
 
 class BalanceSnapshotter:
     """
-    BalanceSnapshotter class, main class of the module used to take and manage snapshots.
+    Main class of the module used to take, compare and manage snapshots.
+
+    Parameters
+    ----------
+    _tokens: List[Union[:class:`brownie.network.contract.Contract`, :class:`str`]]
+        List of tokens in either string from or badger token form.
+    _accounts: List[Union[:class:`brownie.network.account.Account`, :class:`str`]]
+        List of tokens in either string form or badger account form.
+    loop: Optional[:class:`asyncio.BaseEventLoop`]
+        Asyncio loop that will be used in the creation of the snapshot.
 
     Attributes
     -----------
@@ -67,12 +106,13 @@ class BalanceSnapshotter:
         List of accounts.
     snaps: List[:class:`dict`]
         List of snapshots which are in dict form {"name": :class:`str`, balances: :class:`Balances`}
-    loop: :class:`asyncio.Loop`
+    loop: :class:`asyncio.BaseEventLoop`
         Asyncio loop used to run snapshot coroutine.
     balance_event: :class:`asyncio.Event`
-        Event used to regulate when snapshot code needs to move forward.
+        Event used to manage the flow of the snap method.
     """
-    def __init__(self, _tokens: list, _accounts: list, *, loop=None):
+
+    def __init__(self, _tokens: list, _accounts: list, *, loop: asyncio.BaseEventLoop = None):
         # convert any raw addresses to proper token objects
         for i, token in enumerate(_tokens):
             if type(token) == str:
@@ -81,24 +121,27 @@ class BalanceSnapshotter:
         # convert any raw addresses to proper account objects
         for i, account in enumerate(_accounts):
             if type(account) == str:
-                _accounts[i] = accounts.at(account, force=True)
+                _accounts[i] = b_accounts.at(account, force=True)
 
         self.tokens: list[Token] = [Token(token) for token in _tokens]
-        self.accounts = _accounts
-        self.snaps = []
+        self.accounts: list = _accounts
+        self.snaps: list[dict] = []
 
-        self.loop = loop if loop else asyncio.get_event_loop()
-        self.balance_event = asyncio.Event()
+        self.loop: asyncio.BaseEventLoop = loop if loop else asyncio.get_event_loop()
+        self.balance_event: asyncio.Event = asyncio.Event()
 
     def add_token(self, token):
         """
-        Adds a token to the list of tokens. If a token is provided as a string, it will be converted to :class:`Token`.
+        Add a token to the list of tokens.
+
+        If a token is provided as a string, it will be converted to :class:`Token`.
 
         Parameters
         -----------
         token: Union[:class:`brownie.network.contract.Contract`, :class:`str`]
             The token that will be added
         """
+        # if provided token is in string format, change it to Token object
         if type(token) == str:
             token = interface.IERC20(token)
             token = Token(token)
@@ -106,7 +149,9 @@ class BalanceSnapshotter:
 
     def add_account(self, account):
         """
-        Adds an account to the list of accounts. If account is provided as a string, it will be converted to an account.
+        Add an account to the list of accounts.
+
+        If account is provided as a string, it will be converted to an account.
 
         Parameters
         -----------
@@ -115,20 +160,28 @@ class BalanceSnapshotter:
         """
         # Convert raw addresses into account objects
         if type(account) == str:
-            account = accounts.at(account, force=True)
+            account = b_accounts.at(account, force=True)
         self.accounts.append(account)
 
-    def snap(self, name="", print_snap=False):
+    def snap(self, name: str = "", print_snap: bool = False) -> dict[str, Union[str, 'Balances']]:
         """
-        Main snap function that will create snap task or will run the loop if needed.
+        Take a snap of the current state of all the tokens of all the accounts.
+
+        Optionally print out the snap in a table form.
 
         Parameters
         -----------
         name: :class:`str`
-            The name that will be assigned to the snap
+            The name that will be assigned to the snap.
         print_snap: :class:`bool`
-            If True, snap will be printed out
+            If True, snap will be printed out.
+
+        Returns
+        -------
+        Dict[:class:`str`, Union[:class:`str`, :class:`Balances`]]
+            The snap that was taken. {"name": name, "balances": Balances}
         """
+        # create a task if the loop is already running, otherwise run the loop until the async function completes.
         if self.loop.is_running():
             self.loop.create_task(self._async_take_snapshot(name))
         else:
@@ -150,11 +203,14 @@ class BalanceSnapshotter:
                 )
             balances.print()
 
-    async def _async_take_snapshot(self, name):
+        return snap
+
+    async def _async_take_snapshot(self, name: str):
         """
         Function that gathers token balances for accounts in an asynchronous manner.
-        The code will gather all the needed balanceOf calls into a list and through asyncio they will all be called
-        at the same time, improving efficiency.
+
+        The function will gather all the needed balanceOf calls into a list and through asyncio gather they will all
+        be called at the same time, improving efficiency.
 
         Parameters
         -----------
@@ -178,8 +234,22 @@ class BalanceSnapshotter:
         self.snaps.append({"name": name, "balances": balances})
         self.balance_event.set()
 
-    def diff_last_two(self):
-        """Prints out the difference between the last 2 snapshots."""
+    def diff_last_two(self, print_diff: bool = True) -> str:
+        """
+        Create difference table of the last 2 snaps.
+
+        Defaults to also printing out the table.
+
+        Parameters
+        ----------
+        print_diff: :class:`bool`
+            If True prints out difference table, otherwise just returns it
+
+        Returns
+        -------
+        :class:`str`
+            Generated difference table.
+        """
         if len(self.snaps) < 2:
             raise Exception("Insufficient snaps have been taken to compare last two")
 
@@ -210,7 +280,11 @@ class BalanceSnapshotter:
 
                 table.append([token.symbol, account, amount])
 
-        print(tabulate(table, headers=["asset", "account", "balance"]))
+        table = tabulate(table, headers=["asset", "account", "balance"])
+        if print_diff:
+            print(table)
+
+        return table
 
 
 class Balances:
@@ -222,6 +296,7 @@ class Balances:
     balances: Dict[:class:`Token`, Dict[:class:`brownie.network.account.Account`, :class:`float`]]
         Dictionary that contains the token balances of all the accounts.
     """
+
     def __init__(self):
         self.balances = {}
 
@@ -243,7 +318,7 @@ class Balances:
         self.balances[token][account] = value
 
     def print(self):
-        """Prints out the all the token balances of all the accounts."""
+        """Print out all the token balances of all the accounts."""
         table = []
         for token, accounts in self.balances.items():
             for account, value in accounts.items():
